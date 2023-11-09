3bb02ddd900c3cc3bf7a79bcebcb002e7563d44d
Eric Liu <liu.eric103@gmail.com>
2023-11-08 20:39:15 -0800

    add data csv file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /**
  * Load data from CSV file asynchronously and render charts
  */
-d3.csv("data/games_merged.csv").then((data) => {
+d3.csv("data/games.csv").then((data) => {
   // Convert columns to numerical values
   data.forEach((d) => {
     Object.keys(d).forEach((attr) => {
