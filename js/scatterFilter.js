8c5614a7fd104aaf245eb74ca5e96b1cf7847213
Eric Liu <liu.eric103@gmail.com>
2023-11-28 15:38:31 -0800

    filter fixes

--- js/scatterFilter.js
+++ js/scatterFilter.js
@@ -5,9 +5,7 @@ class ScatterFilter {
     this.scatterMatrix = _scatterMatrix;
 
     this.filters = {
-      dateRange: [new Date(1970, 1, 1), new Date()],
-      rating: null,
-      price: [0, 999.99],
+      date: null,
       metacriticScore: [0, 100],
     };
 
@@ -18,22 +16,14 @@ class ScatterFilter {
     const filterPanel = d3.select(this.config.parentElement);
 
     // Rating filter
-    const ratingFilter = filterPanel.append("div").attr("class", "filter");
-    ratingFilter.append("label").text("Release Date:").append("br");
-    ratingFilter
+    const dateFilter = filterPanel.append("div").attr("class", "filter");
+    dateFilter.append("label").text("Release Date:").append("br");
+    dateFilter
       .append("select")
-      .attr("id", "rating")
+      .attr("id", "date")
       .on("change", () => this.updateFilters())
       .selectAll("option")
-      .data([
-        "Overwhelmingly Positive",
-        "Very Positive",
-        "Mostly Positive",
-        "Positive",
-        "Mixed",
-        "Negative",
-        "Mostly Negative",
-      ])
+      .data(["Q1", "Q2", "Q3", "Q4"])
       .enter()
       .append("option")
       .text((d) => d);
@@ -59,16 +49,8 @@ class ScatterFilter {
 
   updateFilters() {
     // Update filter values based on user input
-    this.filters.dateRange = [
-      new Date(d3.select("#startDate").property("value")),
-      new Date(d3.select("#endDate").property("value")),
-    ];
-    this.filters.rating =
-      d3.select("#rating").property("value") || "Overwhelmingly Positive";
-    this.filters.price = [
-      parseFloat(d3.select("#priceRange").property("value")),
-      1000.0,
-    ];
+    this.filters.date =
+      d3.select("#date").property("value") || "Overwhelmingly Positive";
     this.filters.metacriticScore = [
       parseInt(d3.select("#metacriticRange").property("value")),
       100,
@@ -91,7 +73,8 @@ class ScatterFilter {
     // console.log("Current Filters:", this.filters);
 
     const filteredData = this.data.filter((d) => {
-      const matchesRating =
+      // TODO: Filter by date
+      const matchesDate =
         this.filters.rating === null ||
         (d.rating &&
           d.rating.toLowerCase() === this.filters.rating.toLowerCase());
@@ -100,9 +83,9 @@ class ScatterFilter {
         d.metacritic_score >= this.filters.metacriticScore[0] &&
         d.metacritic_score <= this.filters.metacriticScore[1];
 
-      return matchesRating && withinMetacriticScoreRange;
+      return matchesDate && withinMetacriticScoreRange;
     });
 
-    this.packLayout.updateFilteredData(filteredData);
+    this.scatterMatrix.updateFilteredData(filteredData);
   }
 }
